1f01a61dea027fa48800dc8fb5e18f2b3096bdc2
BrandonWARKNAT <brandon_break@live.com.mx>
2017-01-22 22:16:49 -0600

    Creating project. Adding files

---
+++ exampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exampleProject
+//
+//  Created by robot-boy on 22/01/2017.
+//  Copyright © 2017 robot-boy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
